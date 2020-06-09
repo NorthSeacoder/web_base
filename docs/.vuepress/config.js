56d0c969b5133ef5742f532e8e2f67087e646a4d
mengpeng <pengmeng@yangqianguan.com>
2020-06-09 21:32:37 +0800

    build

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -1,28 +1,95 @@
 module.exports = {
     base: '/web-base/',
-    title: 'Base',
+    title: '基础',
     description: 'Just playing around',
     head: [['link', {rel: 'icon', href: '/web-base.png'}]],
     markdown: {
         lineNumbers: true
     },
     themeConfig: {
+      logo: '/web-base.png',
         nav: [
-          { text: 'Home', link: '/' },
-          { text: '数据结构', link: '/data-structure/' },
-          { text: '操作系统', link: '/operating-system/' },
-          { text: '计算机网络', link: '/computer-networks/' },
-          { text: '计算机组成原理', link: '/computer-organization/' },
+          {text: 'Home', link: '/'},
+            {
+                text: '基础',
+                ariaLabel: 'Language Menu',
+                items: [
+                  {text: '数据结构', link: '/base/data-structure/'},
+                  // {text: '操作系统', link: '/base/operating-system/'},
+                  // {text: '计算机网络', link: '/base/computer-networks/'},
+                  // {text: '计算机组成原理', link: '/base/computer-organization/'}
+                ]
+            },
+            {
+                text: '笔记',
+                ariaLabel: 'Language Menu',
+                items: [
+                  {text: '数据结构', link: '/note/data-structure/'},
+                  // {text: '操作系统', link: '/base/operating-system/'},
+                  // {text: '计算机网络', link: '/base/computer-networks/'},
+                  // {text: '计算机组成原理', link: '/base/computer-organization/'}
+                ]
+            },
+            { text: 'Github', link: 'https://github.com/NorthSeacoder'},
+            
         ],
         sidebar: {
-          '/data-structure/':[{
-            title:'数据结构',
-            collapsable: false,
-            children:[
-              'start',
-            ]
-          }]
+            '/base/data-structure/': getGuideSidebar(),
+            '/base/computer-networks/': [
+                {
+                    title: '计算机网络',
+                    collapsable: false,
+                    children: ['start-1']
+                }
+            ],
+
+            '/note/data-structure/': [
+              {
+                title: '绪论',
+                collapsable: false,
+                children: ['start-1']
+              }
+            ],
         }
-        
-      }
+    }
 };
+
+function getGuideSidebar () {
+    return [
+      {
+        title: '链表',
+        collapsable: false,
+        children: [
+          'LinkedList'
+        ]
+      },
+      {
+        title: '栈',
+        collapsable: false,
+        children: [
+          'Stack'
+        ]
+      },
+      {
+        title: '队列',
+        collapsable: false,
+        children: [
+          'Queue'
+        ]
+      },
+      {
+        title: '树',
+        collapsable: false,
+        children: [
+          'Tree'
+        ]
+      },
+      {
+        title: '图',
+        collapsable: false,
+        children: [
+          'Graph'
+        ]
+      },
+    ]
+  }
---
+++ docs/base/data-structure/LinkedList.md
@@ -0,0 +1,18 @@
+# 单链表
+## 实现
+```js
+//链表节点
+class Node {
+    constructor(element){
+        this.element=element;
+        this.next=null
+    }
+}
+
+class LinkedList {
+    constructor(){
+        this.head=null;
+        this.length=0
+    }
+}
+```
