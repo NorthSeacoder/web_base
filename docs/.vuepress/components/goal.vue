2ad5a30d0ec265c6945c26e09c5cb0deb183e56d
mengpeng <pengmeng@yangqianguan.com>
2020-08-06 13:54:55 +0800

    satsh

---
+++ docs/.vuepress/components/goal.vue
@@ -0,0 +1,27 @@
+<template>
+    <div id="mindGraph">
+    </div>
+</template>
+
+<script>
+export default {
+    data() {
+        return {
+            thisPages: {}
+        }
+    },
+    mounted() {
+        const {$page: {path, title}, $site: {pages}} = this;
+        const regex = new RegExp(path);
+        this.thisPages = pages.filter(item => regex.test(item.path) && path !== item.path);
+        const data = {
+            id: title,
+            children: []
+        }
+        
+    },
+}
+</script>
+
+<style lang="scss" scoped>
+</style>
