4719ea286bb4724ec68bc24be98dda254b78a5cc
mengpeng <646388689@qq.com>
2020-08-14 09:51:38 +0800

    add cycle linkedlist

--- docs/base/data-structure/common-data-structure/demo.js
+++ docs/base/data-structure/common-data-structure/demo.js
@@ -7,105 +7,31 @@ class Node {
 }
 
 class LinkedList {
-    constructor(value) {
-        this.head = new Node(value);
+    constructor() {
+        this.head = new Node('head');
     }
-    //按值查询
-    queryByValue(value) {
-        let cur = this.head;
-        while (cur && cur.value !== value) {
-            cur = cur.next;
-        }
-        return cur;
-    }
-    //按index查询(从0开始,0=>head)
-    queryByIndex(index) {
-        let cur = this.head;
-        let sum = 0;
-        while (cur && sum !== index) {
-            cur = cur.next;
-            sum++;
-        }
-        // for (let i = index; i > 0; i--) {
-        //     if (cur && cur.next) {
-        //         cur = cur.next;
-        //     } else {
-        //         cur = null;
-        //     }
-        // }
-        return cur;
-    }
-    //获取node个数
-    length() {
-        let cur = this.head,
-            num = 0;
-        while (cur) {
-            num++;
-            cur = cur.next;
-        }
-        return num;
-    }
-    //末尾新增
-    append(value) {
-        const newNode = new Node(value);
-        let cur = this.head;
-
-        while (cur.next) {
-            cur = cur.next;
-        }
-        cur.next = newNode;
-    }
-    //指定元素(值)后插入
-    insert(value, target) {
-        const newNode = new Node(value);
-        const targetNode = this.queryByValue(target);
-        if (targetNode) {
-            newNode.next = targetNode.next;
-            targetNode.next = newNode;
-        }
-    }
-    //展示所有节点
+    //增
+    append(){}
+    //删
+    delete(){}
+    //改
+    update(){}
+    //查
+    query(){}
+    //获取所有
     display() {
         const res = [];
         let cur = this.head;
-        while (cur) {
+        while (cur&&cur.value!=='head') {
             res.push(cur.value);
             cur = cur.next;
         }
         return res.join('=>');
     }
-    //按值修改
-    updateByValue(value, newValue) {
-        const target = this.queryByValue(value);
-        target.value = newValue;
-    }
-    //按index修改
-    updateByIndex(index, newValue) {
-        const target = this.queryByIndex(index);
-        target.value = newValue;
-    }
-    //查找前一个
-    findPrev(item) {
-        let cur = this.head;
-        while (cur.next !== null && cur.next.value !== item) {
-            cur = cur.next;
-        }
-        if (cur.next === null) {
-            return null;
-        }
-        return cur;
-    }
-    //按值删除
-    deleteByValue(value) {
-        let pre = this.findPrev(value);
-        if (pre) {
-            pre.next = pre.next.next;
-        }
-    }
 }
 
 //test
-const list = new LinkedList('head');
+const list = new LinkedList();
 list.append('test1');
 list.append('test2');
 list.append('test3');
